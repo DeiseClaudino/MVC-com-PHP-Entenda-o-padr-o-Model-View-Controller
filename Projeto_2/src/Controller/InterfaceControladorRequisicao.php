05502508b16ad43ccec3638f0ad48c01d62da580
DeiseClaudino <deise.claudino98@gmail.com>
2020-08-04 22:36:35 -0300

    Representando melhor o HTTP

---
+++ Projeto_2/src/Controller/InterfaceControladorRequisicao.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Alura\Cursos\Controller;
+
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+interface InterfaceControladorRequisicao
+{
+    public function processaRequisicao(ServerRequestInterface $request): ResponseInterface;
+
+}
